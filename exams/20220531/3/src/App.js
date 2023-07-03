33bb322e7136820e7d63dfae9b59d16414b4261d
papum_debian <danieledugo1@gmail.com>
2023-07-03 13:18:46 +0200

    removed submodule

---
+++ exams/20220531/3/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+	const [workAvailable, setWorkAvailable] = useState(null);
+
+	return (
+		<div>
+			{ workAvailable && 
+				<
+			}
+		</div>
+	);
+}
+
+export default App;
